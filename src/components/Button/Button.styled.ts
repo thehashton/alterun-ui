0d5b7fc02497cbdc67762c2e44eb536c474078ec
Harry Ghazni <hghazni@gmail.com>
2022-11-11 03:05:57 +0000

    initial commit

---
+++ src/components/Button/Button.styled.ts
@@ -0,0 +1,6 @@
+import { Button } from "@mui/material";
+import { styled } from '@mui/material/styles';
+
+export const ButtonAlt = styled(Button)`
+  // style goes here
+`;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ButtonAlt } from './Button.styled';
+
+export const Button = () => {
+    return (
+        <ButtonAlt variant="contained">test</ButtonAlt>
+    )
+}
